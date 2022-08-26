dfe962baf39de1ff664619fa29dc07648c0dbba1
Jack Coleman <jackcoleman@Jacks-MacBook-Pro-2.local>
2022-08-26 13:31:52 -0400

    test

--- lib/puppet/type/vault_policy.rb
+++ lib/puppet/type/vault_policy.rb
@@ -6,6 +6,21 @@ Puppet::Type.newtype(:vault_policy) do
 
   ensurable
 
+  def rules_to_string(rules)
+    Puppet.debug("Entering rules_to_string with rules: #{rules}")
+    template = %q{
+# comment
+<% rules.each do |rule| %>
+path "<%= rule['path'] %>" {
+    capabilities = <%= rule['capabilities'] %>
+}
+<% end %>
+    }.gsub(/^  /, '')
+    message = ERB.new(template).result(binding)
+    Puppet.debug("message is: #{message}")
+    message
+  end
+
   newparam(:name) do
     desc 'The name assigned to the vault policy.'
 
@@ -14,5 +29,8 @@ Puppet::Type.newtype(:vault_policy) do
 
   newproperty(:rules, array_matching: :all) do
     desc 'The content of the vault policy'
+    munge do |value|
+        rules_to_srting(value)
+    end
   end
 end
