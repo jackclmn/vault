88adf0d82db6c6574ab134f787939ef241cce4f4
Jack Coleman <jackcoleman@Jacks-MacBook-Pro-2.local>
2022-08-26 11:45:10 -0400

    test

--- lib/puppet/provider/vault_policy/vault.rb
+++ lib/puppet/provider/vault_policy/vault.rb
@@ -15,7 +15,7 @@ Puppet::Type.type(:vault_policy).provide(:vault) do
             }
   
         % end
-    }
+    }.gsub(/^  /, '')
     message = ERB.new(template)
     r = rules
     result = message.result
