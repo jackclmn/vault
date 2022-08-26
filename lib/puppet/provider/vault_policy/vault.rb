15e6c5bd5e518591483e9fbe99c7bd201d9f3e03
Jack Coleman <jackcoleman@Jacks-MacBook-Pro-2.local>
2022-08-26 12:55:55 -0400

    add debug

--- lib/puppet/provider/vault_policy/vault.rb
+++ lib/puppet/provider/vault_policy/vault.rb
@@ -1,5 +1,6 @@
 require 'vault'
 require 'erb'
+require 'puppet'
 
 Puppet::Type.type(:vault_policy).provide(:vault) do
   desc 'This provider manages vault policies.'
@@ -8,6 +9,7 @@ Puppet::Type.type(:vault_policy).provide(:vault) do
   confine    kernel: :Linux
 
   def rules_to_string(rules)
+    Puppet.debug("Entering rules_to_string with rules: #{rules}")
     template = %q{
         <% rules.each do |v| %>
             path "<%= v %>" {
@@ -17,26 +19,32 @@ Puppet::Type.type(:vault_policy).provide(:vault) do
         <% end %>
     }.gsub(/^  /, '')
     message = ERB.new(template).result(binding)
+    Puppet.debug("message is: #{message}")
     message
   end
 
   def exists?
+    Puppet.debug('entering exists?')
     Vault.sys.policies.include? resource[:name]
   end
 
   def rules
+    Puppet.debug('entering rules')
     Vault.sys.policy(resource[:name]).rules
   end
 
   def rules=(value)
+    Puppet.debug("entering rules=value with name: #{resource['name']} and rules #{resource['rules']}")
     Vault.sys.put_policy(resource[:name], rules_to_string(resource[:rules]))
   end
 
   def create
+    Puppet.debug("entering create with name: #{resource['name']} and rules #{resource['rules']}")
     Vault.sys.put_policy(resource[:name], rules_to_string(resource[:rules]))
   end
 
   def destroy
+    Puppet.debug("entering destroy with name: #{resource['name']} and rules #{resource['rules']}")
     Vault.sys.delete_policy(resource[:name])
   end
 end
